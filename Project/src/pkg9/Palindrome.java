63f5ee75d91d60276c08486fb01d56a61a5896a6
ahujar <ahujar@L70579.ali.local>
2022-03-10 02:56:36 +0530

    Palindrome program added

--- Project/src/pkg9/Palindrome.java
+++ Project/src/pkg9/Palindrome.java
@@ -1,5 +1,36 @@
 package pkg9;
 
-public class Palindrome {
+import java.util.Scanner;
 
+//Palindrome program
+public class Palindrome 
+{
+	public static void palin(int num)
+	{
+		int temp=num;
+		int rev=0;
+		while(num>0)
+		{
+			int rem=num%10;
+			rev=(rev*10)+rem;
+			num=num/10;
+		}
+		if(temp==rev)
+		{
+			System.out.println("Palindrome number");
+		}
+		else
+		{
+			System.out.println("Not a Palindrome number");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Palindrome Program");//121-->121(Palindrome), 123-->321Not Palindrome
+		System.out.println("Enter value to be checked");
+		Scanner s = new Scanner(System.in);
+		int p=s.nextInt();
+		Palindrome.palin(p);
+		
+	}
 }
